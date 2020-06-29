730b15b2bc3d80e73d679f9a26a89d24a067f462
SamyOteroGlez <otero.glez.samy@gmail.com>
2020-06-29 15:39:26 -0400

    Add keywords in spanish

--- resources/views/_meta_tags.blade.php
+++ resources/views/_meta_tags.blade.php
@@ -1,6 +1,9 @@
 <meta charset="utf-8">
-<meta name="description" content="Stats from clans and players of Clash Royale">
-<meta name="keywords" content="clash royal, clash royale deck, clash royale arena, clash royale chest, clash royale clans, clash royale game, play clash royale">
+<meta name="description" content="Stats from clans and players of Clash Royale.">
+<meta name="keywords"
+    content="clash royal, clash royale deck, clash royale mazo, clash royale arena, clash royale chest,
+            clash royale cofre, , clash royale clanes, clash royale game, clash royale juego,
+            play clash royale, juega clash royale">
 <meta name="author" content="Cloverpath Solutions">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
